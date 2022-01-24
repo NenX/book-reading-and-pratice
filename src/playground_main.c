182273f9cde368ddeed7f4df27c7b234a16e3baa
brainfuck <brainfuck@lian-med.com>
2022-01-24 18:08:45 +0800

    feat: strerror that is not thread-safe

--- src/playground_main.c
+++ src/playground_main.c
@@ -6,57 +6,62 @@ pthread_mutex_t mutex;
 pthread_cond_t con;
 
 pthread_cond_t empty_con;
-
 static volatile long count;
+pthread_key_t t1;
+pthread_key_t t2;
+pthread_once_t once;
+
+void destrctor(void *data)
+{
+    printf("destrctor : %d\n", (int)data);
+}
+
+void fn(void)
+{
+    printf("once !!\n");
+    pthread_key_create(&t1, destrctor);
+    pthread_key_create(&t2, destrctor);
+}
 static void *
 threadFunc(void *arg)
 {
+
+    pthread_once(&once, fn);
     long n = (long)arg;
-    while (1)
+
+    if (n == 111)
     {
-
-        pthread_mutex_lock(&mutex);
-
-        if (count > 100000)
-        {
-            printf("peer thread %ld: %ld\n", n, count);
-            pthread_cond_signal(&con);
-            pthread_cond_wait(&empty_con, &mutex);
-        }
-        count++;
-
-        pthread_mutex_unlock(&mutex);
+        pthread_setspecific(t2, (void *)777);
+        void *data = pthread_getspecific(t2);
+        printf("thread specific data %ld:%u:%ld\n", n, t2, (int)data);
+    }
+    else
+    {
+        void *data = pthread_getspecific(t2);
+        printf("thread specific data %ld:%u:%ld\n", n, t2, (int)data);
     }
 
+    printf("thread %ld:%u-%u\n", n, t1, t2);
     return NULL;
 }
+
 int main(int argc, char *argv[])
 {
+    printf("main 123\n");
+
     pthread_t t1;
     pthread_t t2;
 
     int s;
-    s = pthread_create(&t1, NULL, threadFunc, (void *)1);
+    s = pthread_create(&t1, NULL, threadFunc, (void *)111);
     if (s != 0)
         errExitEN(s, "pthread_create");
-    s = pthread_create(&t2, NULL, threadFunc, (void *)2);
+    s = pthread_create(&t2, NULL, threadFunc, (void *)222);
     if (s != 0)
         errExitEN(s, "pthread_create");
 
     if (s != 0)
         errExitEN(s, "pthread_join");
-
-    while (1)
-    {
-        pthread_mutex_lock(&mutex);
-        while (count <= 0)
-        {
-            pthread_cond_wait(&con, &mutex);
-        }
-        count = 0;
-        printf("main thread consumes %ld \n", count);
-        sleep(1);
-        pthread_cond_signal(&empty_con);
-        pthread_mutex_unlock(&mutex);
-    }
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
 }
