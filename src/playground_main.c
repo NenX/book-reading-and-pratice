4cb00687c58a730b6390332b65866d55c6ff05c8
brainfuck <brainfuck@lian-med.com>
2022-01-25 19:21:18 +0800

    the Universal I/O Model

--- src/playground_main.c
+++ src/playground_main.c
@@ -1,71 +1,29 @@
 #include <pthread.h>
 #include "tlpi_hdr.h"
 #include <unistd.h>
-
-pthread_mutex_t mutex;
-pthread_cond_t con;
-
-pthread_cond_t empty_con;
-static volatile long count;
-pthread_key_t t1;
-pthread_key_t t2;
-pthread_once_t once;
-
-void destrctor(void *data)
-{
-    printf("destrctor : %d\n", (int)data);
-}
-void abc(void *data)
-{
-    printf("destrctor : %d\n", (int)data);
-}
-void fn(void)
-{
-    printf("once !!\n");
-    pthread_key_create(&t1, destrctor);
-    pthread_key_create(&t2, destrctor);
-}
-static void *
-threadFunc(void *arg)
-{
-
-    pthread_once(&once, fn);
-    long n = (long)arg;
-
-    if (n == 111)
-    {
-        pthread_setspecific(t2, (void *)777);
-        void *data = pthread_getspecific(t2);
-        printf("thread specific data %ld:%u:%ld\n", n, t2, (int)data);
-    }
-    else
-    {
-        void *data = pthread_getspecific(t2);
-        printf("thread specific data %ld:%u:%ld\n", n, t2, (int)data);
-    }
-
-    printf("thread %ld:%u-%u\n", n, t1, t2);
-    return NULL;
-}
-
+#include <fcntl.h>
+#define MAX_NUM 9999
+char buf[MAX_NUM];
 int main(int argc, char *argv[])
 {
-    printf("main 123\n");
-    pthread_cleanup_push(abc, (void *)9999999);
-    pthread_cleanup_pop(2);
-    pthread_t t1;
-    pthread_t t2;
+        printf("main start\n");
 
-    int s;
-    s = pthread_create(&t1, NULL, threadFunc, (void *)111);
-    if (s != 0)
-        errExitEN(s, "pthread_create");
-    s = pthread_create(&t2, NULL, threadFunc, (void *)222);
-    if (s != 0)
-        errExitEN(s, "pthread_create");
+    size_t result;
+    int fd;
+    fd = open("../.gitignore", O_RDONLY);
+    if (fd == -1)
+    {
+        errExit("open");
+    }
+    while ((result = read(STDIN_FILENO, buf, MAX_NUM)) >= 0)
+    {
+        buf[result] = '\0';
+        printf("num:%ld, data: %s", result, buf);
 
-    if (s != 0)
-        errExitEN(s, "pthread_join");
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
+        if(result==0){
+            pthread_exit(NULL);
+        }
+    }
+
+
 }
