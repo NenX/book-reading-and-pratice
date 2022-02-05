9d0548e446e78fecf4bc599eda87ba87f139983a
noah <noah1999@qq.com>
2022-02-05 21:05:42 +0800

    Time

--- src/playground_main.c
+++ src/playground_main.c
@@ -2,28 +2,10 @@
 #include "tlpi_hdr.h"
 #include <unistd.h>
 #include <fcntl.h>
+#include "curr_time.h"
 #define MAX_NUM 9999
 char buf[MAX_NUM];
 int main(int argc, char *argv[])
 {
-        printf("main start\n");
-
-    size_t result;
-    int fd;
-    fd = open("../.gitignore", O_RDONLY);
-    if (fd == -1)
-    {
-        errExit("open");
-    }
-    while ((result = read(STDIN_FILENO, buf, MAX_NUM)) >= 0)
-    {
-        buf[result] = '\0';
-        printf("num:%ld, data: %s", result, buf);
-
-        if(result==0){
-            pthread_exit(NULL);
-        }
-    }
-
-
+    printf("result: %s\n", currTime(argv[1]));
 }
