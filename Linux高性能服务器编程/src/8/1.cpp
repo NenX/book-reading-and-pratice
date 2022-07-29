2fc7af47cc661d28a0f9131b3212ac8ed9fef35a
brainfuck <lixiaofan.dev@gmail.com>
2022-07-29 19:00:46 +0800

    HTTP 请求的读取和分析

--- Linux高性能服务器编程/src/8/1.cpp
+++ Linux高性能服务器编程/src/8/1.cpp
@@ -3,7 +3,16 @@
  * HTTP 请求的读取和分析
  */
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <assert.h>
 
+#define BUFFER_SIZE 4096
 enum CHECK_STATE
 {
     CHECK_STATE_REQUEST_LINE = 0,
@@ -33,14 +42,232 @@ LINE_STATUS parse_line(char *buffer, int &checked_index, int &read_index)
     char temp;
     for (checked_index; checked_index < read_index; checked_index++)
     {
-        /* code */
+        temp = buffer[checked_index];
+        if (temp = '\r')
+        {
+            if ((checked_index + 1) == read_index)
+            {
+                return LINE_OPEN;
+            }
+            else if (buffer[checked_index + 1] == '\n')
+            {
+                buffer[checked_index++] = '\0';
+                buffer[checked_index++] = '\0';
+                return LINE_OK;
+            }
+
+            return LINE_BAD;
+        }
+        else if (temp = '\n')
+        {
+            if ((checked_index > 1) && buffer[checked_index - 1] == '\r')
+            {
+                buffer[checked_index - 1] = '\0';
+                buffer[checked_index++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
+        }
     }
-    
+
     return LINE_OPEN;
 }
-
-int main()
+HTTP_CODE parse_requestline(char *temp, CHECK_STATE &checkstate)
 {
-    printf("hello world 222\n");
-    
+    // for: "GET http://abc.com/cdf HTTP/1.1 ..."
+    // url-> " http://abc.com/cdf HTTP/1.1 ..."
+    char *url = strpbrk(temp, " \t");
+    if (!url)
+    {
+        return BAD_REQUEST;
+    }
+
+    // url-> "http://abc.com/cdf HTTP/1.1 ..."
+    *url++ = '\0';
+
+    // method-> "GET"
+    char *method = temp;
+
+    if (strcasecmp(method, "GET") == 0)
+    {
+        printf("The request method is GET.\n");
+    }
+    else
+    {
+        return BAD_REQUEST;
+    }
+
+    // url-> "http://abc.com/cdf HTTP/1.1 ..."
+    url += strspn(url, " \t");
+
+    // version-> " HTTP/1.1 ..."
+    char *version = strpbrk(url, " \t");
+
+    if (!version)
+    {
+        return BAD_REQUEST;
+    }
+
+    // version-> "HTTP/1.1 ..."
+    // url -> "http://abc.com/cdf"
+    *version++ = '\0';
+
+    version += strspn(version, " \t");
+
+    if (strcasecmp(version, "HTTP/1.1") != 0)
+    {
+        return BAD_REQUEST;
+    }
+
+    if (strncasecmp(url, "http://", 7) == 0)
+    {
+        // url-> "abc.com/cdf"
+        url += 7;
+        // url-> "/cdf"
+        url = strchr(url, '/');
+    }
+
+    if (!url || url[0] != '/')
+    {
+        return BAD_REQUEST;
+    }
+    printf("The request URL is: %s\n", url);
+
+    checkstate = CHECK_STATE_HEADER;
+
+    return NO_REQUEST;
+}
+
+HTTP_CODE parse_headers(char *temp)
+{
+    if (temp[0] == '\0')
+    {
+        return GET_REQUEST;
+    }
+    else if (strncasecmp(temp, "Host:", 5) == 0)
+    {
+        temp += 5;
+        temp += strspn(temp, " \t");
+        printf("The request host is %s\n", temp);
+    }
+    else
+    {
+        printf("I can't handle this header\n");
+    }
+    return NO_REQUEST;
+}
+HTTP_CODE parse_content(char *buffer, int &checked_index, CHECK_STATE &checkstate, int &read_index, int &start_line)
+{
+    LINE_STATUS linestatus = LINE_OK;
+    HTTP_CODE retcode = NO_REQUEST;
+
+    while ((linestatus = parse_line(buffer, checked_index, read_index)) == LINE_OK)
+    {
+        char *temp = buffer + start_line;
+        start_line = checked_index;
+        switch (checkstate)
+        {
+        case CHECK_STATE_REQUEST_LINE:
+            retcode = parse_requestline(temp, checkstate);
+            if (retcode == BAD_REQUEST)
+            {
+                return BAD_REQUEST;
+            }
+            break;
+        case CHECK_STATE_HEADER:
+            retcode = parse_headers(temp);
+            if (retcode == BAD_REQUEST)
+            {
+                return BAD_REQUEST;
+            }
+            else if (retcode == GET_REQUEST)
+            {
+                return GET_REQUEST;
+            }
+
+            break;
+
+        default:
+            return INTERNAL_ERROR;
+        }
+    }
+
+    return NO_REQUEST;
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc <= 2)
+    {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
+    }
+    const char *const ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
+
+    int ret = bind(listenfd, (struct sockaddr *)&address, sizeof(struct sockaddr));
+    assert(ret != -1);
+
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+
+    struct sockaddr_in client_address;
+    socklen_t client_addrlen = sizeof(struct sockaddr_in);
+
+    int fd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlen);
+    assert(fd >= 0);
+
+    char buffer[BUFFER_SIZE];
+    memset(buffer, '\0', BUFFER_SIZE);
+
+    int data_read = 0;
+    int checked_index = 0;
+    int read_index = 0;
+    int start_line = 0;
+
+    CHECK_STATE checkstate = CHECK_STATE_REQUEST_LINE;
+    while (1)
+    {
+        data_read = recv(fd, buffer + read_index, BUFFER_SIZE - read_index, 0);
+        if (data_read == -1)
+        {
+            printf("reading failed.\n");
+            break;
+        }
+        else if (data_read == 0)
+        {
+            printf("remote client has closed the connection.\n");
+            break;
+        }
+        read_index += data_read;
+
+        HTTP_CODE result = parse_content(buffer, checked_index, checkstate, read_index, start_line);
+
+        if (result == NO_REQUEST)
+        {
+            continue;
+        }
+        else if (result == GET_REQUEST)
+        {
+            send(fd, szret[0], strlen(szret[0]), 0);
+            break;
+        }
+        else
+        {
+            send(fd, szret[1], strlen(szret[1]), 0);
+            break;
+        }
+    }
+
+    close(fd);
+    close(listenfd);
+    return 0;
 }
