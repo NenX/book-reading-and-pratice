c67c5a1c5cce864355fad3cbbd3650d3d60e6ef2
brainfuck <noah1999@qq.com>
2024-08-31 18:42:33 +0800

    fix

--- rust_pg/test_ffi/c/add.c
+++ rust_pg/test_ffi/c/add.c
@@ -4,7 +4,7 @@
 int32_t add(int32_t a, int32_t b)
 {
     int32_t result = a + b;
-    printf("[C] source: Argument a is: %i, Argument b is:%i \n", a, b);
+    printf("[C] source: Argument a is:{ %i }, Argument b is:{ %i } \n", a, b);
     printf("[C] source: returning the result { %i } to Rust\n", result);
     return result;
 }
