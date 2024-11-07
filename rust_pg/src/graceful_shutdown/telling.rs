707aa272f7459f47478215bf05e8b8c2647fb7e5
noah <noah@gmail.com>
2024-11-07 14:21:52 +0800

    refactor: graceful shutdown

--- rust_pg/src/graceful_shutdown/telling.rs
+++ rust_pg/src/graceful_shutdown/telling.rs
@@ -1,57 +1,84 @@
-
 pub async fn telling_impl_1() {
-    // Step 1: Create a new CancellationToken
     let token = tokio_util::sync::CancellationToken::new();
-    let h;
-    // Step 2: Clone the token for use in another task
-    let cloned_token = token.clone();
-    h = tokio::spawn(async move {
-        eprintln!("telling_impl_1 will sleep");
-        tokio::select! {
-            // Step 3: Using cloned token to listen to cancellation requests
-            _ = cloned_token.cancelled() => {
-                // The token was cancelled, task can shut down
-            }
-            _ = tokio::time::sleep(std::time::Duration::from_secs(9999)) => {
-                // Long work has completed
-            }
-        }
-    });
+    let mut hv = vec![];
 
-    // Task 2 - Cancel the original token after a small delay
+    for i in 0..2 {
+        let cloned_token = token.clone();
+
+        let h = tokio::spawn(async move {
+            eprintln!("telling_impl_1 {i} will sleep");
+            tokio::select! {
+                _ = cloned_token.cancelled() => eprintln!("telling_impl_1 {i} woke"),
+                _ = tokio::time::sleep(std::time::Duration::from_secs(9999)) => { }
+            }
+        });
+        hv.push(h);
+    }
+
     tokio::spawn(async move {
         tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-
-        // Step 4: Cancel the original or cloned token to notify other tasks about shutting down gracefully
+        println!("telling_impl_1 wake");
         token.cancel();
     });
-    h.await.unwrap();
+    for h in hv {
+        h.await.unwrap();
+    }
     println!("telling_impl_1 end");
 }
 pub async fn telling_impl_2() {
     let (sender, _) = tokio::sync::broadcast::channel::<i32>(1);
-    let h;
+    let mut hv = vec![];
 
-    let mut x = sender.subscribe();
+    for i in 0..2 {
+        let mut x = sender.subscribe();
 
-    h = tokio::spawn(async move {
-        eprintln!("telling_impl_2  will sleep");
-        tokio::select! {
-            _ = x.recv() => {
-                // The token was cancelled, task can shut down
+        let h = tokio::spawn(async move {
+            eprintln!("telling_impl_2 {i} will sleep");
+            tokio::select! {
+                _ = x.recv() => eprintln!("telling_impl_1 {i} woke"),
+                _ = tokio::time::sleep(std::time::Duration::from_secs(9999)) => { }
             }
-            _ = tokio::time::sleep(std::time::Duration::from_secs(9999)) => {
-                // Long work has completed
-            }
-        }
-    });
+        });
+        hv.push(h);
+    }
 
     tokio::spawn(async move {
         tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        println!("telling_impl_2 wake");
         drop(sender); //这里需要 drop
     });
-    h.await.unwrap();
+    for h in hv {
+        h.await.unwrap();
+    }
     println!("telling_impl_2 end");
+}
+pub async fn telling_impl_3() {
+    let note = std::sync::Arc::new(tokio::sync::Notify::new());
+    let mut hv = vec![];
+
+    for i in 0..2 {
+        let note2 = note.clone();
+
+        let h = tokio::spawn(async move {
+            eprintln!("telling_impl_3 {i} will sleep");
+            tokio::select! {
+                _ = note2.notified() => eprintln!("telling_impl_1 {i} woke"),
+                _ = tokio::time::sleep(std::time::Duration::from_secs(9999)) => { }
+            }
+        });
+        hv.push(h);
+    }
+
+    tokio::spawn(async move {
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        println!("telling_impl_3 wake");
+
+        note.notify_waiters();
+    });
+    for h in hv {
+        h.await.unwrap();
+    }
+    println!("telling_impl_3 end");
 }
 
 #[tokio::test]
@@ -59,4 +86,5 @@ pub async fn telling_impl_2() {
 async fn test() {
     telling_impl_1().await;
     telling_impl_2().await;
+    telling_impl_3().await;
 }
